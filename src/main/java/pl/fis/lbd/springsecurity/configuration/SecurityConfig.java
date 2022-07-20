03ba8853b78977acad9c11724cec63dc2d63f457
sylwekfeduk <75621927+sylwekfeduk@users.noreply.github.com>
2022-07-20 11:43:52 +0200

    Exercise from 9 to 13

--- src/main/java/pl/fis/lbd/springsecurity/configuration/SecurityConfig.java
+++ src/main/java/pl/fis/lbd/springsecurity/configuration/SecurityConfig.java
@@ -1,9 +1,18 @@
 package pl.fis.lbd.springsecurity.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -19,5 +28,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .formLogin();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder()
+                .password(passwordEncoder().encode("admin"))
+                .username("admin")
+                .roles("ADMIN", "USER")
+                .build();
+        UserDetails user = User.builder()
+                .password(passwordEncoder().encode("user"))
+                .username("user")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
     }
 }
